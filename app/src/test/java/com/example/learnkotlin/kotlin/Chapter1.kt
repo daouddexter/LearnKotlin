53ca343777bc83df24441e96f4209130a70ddd4b
mohammeddaoud <mohdaou1@in.ibm.com>
2020-08-22 17:30:21 +0530

    Chapter 1

---
+++ app/src/test/java/com/example/learnkotlin/kotlin/Chapter1.kt
@@ -0,0 +1,36 @@
+package com.example.learnkotlin.kotlin
+
+val i = 1 // Automatic resolution as Int type
+val j = 'a' // Automatic resolution to Char type
+val k = 20.0 // Automatic resolution to Double
+val l = 20.0f // Automatic resolution to float
+val m = "Hello" //Automatic resolution to String
+
+
+/*Important concepts:
+
+Function types, allowing functions to receive other functions as parameters or return other functions
+Lambda expressions, letting you pass around blocks of code with minimum boilerplate
+Data classes, providing a concise syntax for creating immutable value objects
+A rich set of APIs in the standard library for working with objects and collections in the functional style
+
+Kotlin is statically typed and supports type inference, allowing it to maintain correctness and
+performance while keeping the source code concise.
+
+Kotlin supports both object-oriented and functional programming styles, enabling higher-level
+abstractions through first-class functions and simplifying testing and multithreaded development
+through the support of immutable values.
+
+It works well for server-side applications, fully supporting all existing Java frameworks and
+providing new tools for common tasks such as HTML generation and persistence.
+
+It works for Android as well, thanks to a compact runtime, special compiler support for
+Android APIs, and a rich library providing Kotlin-friendly functions for common Android development
+ tasks.
+
+It’s free and open source, with full support for the major IDEs and build systems.
+
+Kotlin is pragmatic, safe, concise, and interoperable, meaning it focuses on using proven solutions
+for common tasks, preventing common errors such as NullPointerExceptions, supporting compact and
+easy-to-read code, and providing unrestricted integration with Java.
+  */
